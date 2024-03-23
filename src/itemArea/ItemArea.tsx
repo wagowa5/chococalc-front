929cd9706804f898efab61d5a814172335f36065
wagowa5 <82742482+wagowa5@users.noreply.github.com>
2024-03-23 20:07:05 +0900

    各アイテムの設置、リセットボタンを追加

--- src/itemArea/ItemArea.tsx
+++ src/itemArea/ItemArea.tsx
@@ -7,7 +7,7 @@ import { MESSAGES, FIELDS } from '../constants/constants';
 import ButtonGroupComponent from './ButtonGroupComponent';
 import ScrollSelect from './ScrollSelect';
 import { 
-    vitaButtonsData, canButtonsData, sealButtonsData,
+    vitaButtonsData, canButtonsData, sealButtonsData, liquidButtonsData,
     hpSpScrollOptions, basicScrollOptions, detailScrollOptions
 } from './itemConfig';
 
@@ -39,71 +39,106 @@ const ItemArea = (
     // 巻物のセレクトボックス
     const [hpScroll, setHpScroll] = React.useState('');
     const [spScroll, setSpScroll] = React.useState('');
+    const [powScroll, setPowScroll] = React.useState('');
+    const [intScroll, setIntScroll] = React.useState('');
+    const [spdScroll, setSpdScroll] = React.useState('');
+    const [vitScroll, setVitScroll] = React.useState('');
+    const [lukScroll, setLukScroll] = React.useState('');
+    const [atkScroll, setAtkScroll] = React.useState('');
+    const [defScroll, setDefScroll] = React.useState('');
+    const [matScroll, setMatScroll] = React.useState('');
+    const [mdfScroll, setMdfScroll] = React.useState('');
 
-    // HP巻物のセレクトボックスの変更時の処理
+    // 巻物のセレクトボックスの変更時の処理
     const handleHpScrollChange = (event: SelectChangeEvent) => {
         setHpScroll(event.target.value);
     };
-
-    // SP巻物のセレクトボックスの変更時の処理
     const handleSpScrollChange = (event: SelectChangeEvent) => {
         setSpScroll(event.target.value);
+    };
+    const handlePowScrollChange = (event: SelectChangeEvent) => {
+        setPowScroll(event.target.value);
+    };
+    const handleIntScrollChange = (event: SelectChangeEvent) => {
+        setIntScroll(event.target.value);
+    };
+    const handleSpdScrollChange = (event: SelectChangeEvent) => {
+        setSpdScroll(event.target.value);
+    };
+    const handleVitScrollChange = (event: SelectChangeEvent) => {
+        setVitScroll(event.target.value);
+    };
+    const handleLukScrollChange = (event: SelectChangeEvent) => {
+        setLukScroll(event.target.value);
+    };
+    const handleAtkScrollChange = (event: SelectChangeEvent) => {
+        setAtkScroll(event.target.value);
+    };
+    const handleDefScrollChange = (event: SelectChangeEvent) => {
+        setDefScroll(event.target.value);
+    };
+    const handleMatScrollChange = (event: SelectChangeEvent) => {
+        setMatScroll(event.target.value);
+    };
+    const handleMdfScrollChange = (event: SelectChangeEvent) => {
+        setMdfScroll(event.target.value);
     };
 
     return (
         <>
-        <Grid container spacing={2}>
-            {/* ビタ */}
-            <Grid item xs={5}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    '& > *': {
-                        m: 1,
-                    },
-                }}
-            >
-                <ButtonGroupComponent buttons={vitaButtonsData} />
-            </Box>
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1} justifyContent="center" alignItems="start">
+            {/* ----- 1行目 ----- */}
+            {/* ビタと缶・シールのリセットボタン */}
+            <Grid xs={1}></Grid>
+            <Grid item xs={3} justifyContent="center" alignItems="center">
+                <Button variant="contained">ビタリセット</Button>
             </Grid>
+            <Grid xs={2}></Grid>
+            <Grid item xs={4} justifyContent="center" alignItems="center">
+                <Button variant="contained">缶・シールリセット</Button>
+            </Grid>
+            <Grid xs={2}></Grid>
+                
+            {/* ----- 2行目 ----- */}
+            <Grid xs={1}></Grid>
+            {/* ビタ */}
+            <Grid item xs={3}>
+                <ButtonGroupComponent buttons={vitaButtonsData} />
+            </Grid>
+            <Grid xs={2}></Grid>
 
             {/* 魔獣缶・シール(かき氷) */}
-            <Grid item xs={7}>
-                <Grid container spacing={2}>
+            <Grid item xs={5}>
+                <Grid container spacing={1}>
                     {/* 魔獣缶 */}
                     <Grid item xs = {6}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            '& > *': {
-                                m: 1,
-                            },
-                        }}
-                        >
                         <ButtonGroupComponent buttons={canButtonsData} />
-                    </Box>
                     </Grid>
 
                     {/* シール(かき氷) */}
                     <Grid item xs = {6}>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            '& > *': {
-                                m: 1,
-                            },
-                        }}
-                        >
                         <ButtonGroupComponent buttons={sealButtonsData} />
-                    </Box>
                     </Grid>
                 </Grid>
             </Grid>
+            <Grid xs={1}></Grid>
 
+            {/* ----- 3行目 ----- */}
+            {/* 巻物とリキッドのリセットボタン */}
+            <Grid xs={1}></Grid>
+            <Grid item xs={3} justifyContent="center" alignItems="center">
+                <Button variant="contained">巻物リセット</Button>
+            </Grid>
+            <Grid xs={3}></Grid>
+            <Grid item xs={5} justifyContent="center" alignItems="center">
+                <Button variant="contained">リキッドリセット</Button>
+            </Grid>
+
+            {/* ----- 4行目 ----- */}
             {/* 巻物 */}
             <Grid item xs={6}>
-                <Grid container spacing={2}>
-                    {/* HP, POW, INT, SPD, VIT, LUK */}
+                <Grid container spacing={0}>
                     {/* HP */}
                     <Grid item xs={6}>
                         <ScrollSelect
@@ -113,25 +148,109 @@ const ItemArea = (
                             onChange={handleHpScrollChange}
                         />
                     </Grid>
-
-                    {/* SP, ATK, DEF, MAT, MDF */}
                     {/* SP */}
                     <Grid item xs={6}>
                         <ScrollSelect
-                            options={hpSpScrollOptions} // この例では、HPとSPで同じオプションを使用しています。必要に応じて変更してください。
+                            options={hpSpScrollOptions}
                             label="SP"
                             selectedValue={spScroll}
                             onChange={handleSpScrollChange}
                         />
                     </Grid>
+                    {/* POW */}
+                    <Grid item xs={4}>
+                        <ScrollSelect
+                            options={basicScrollOptions}
+                            label="POW"
+                            selectedValue={powScroll}
+                            onChange={handlePowScrollChange}
+                        />
+                    </Grid>
+                    {/* INT */}
+                    <Grid item xs={4}>
+                        <ScrollSelect
+                            options={basicScrollOptions}
+                            label="INT"
+                            selectedValue={intScroll}
+                            onChange={handleIntScrollChange}
+                        />
+                    </Grid>
+                    {/* VIT */}
+                    <Grid item xs={4}>
+                        <ScrollSelect
+                            options={basicScrollOptions}
+                            label="VIT"
+                            selectedValue={vitScroll}
+                            onChange={handleVitScrollChange}
+                        />
+                    </Grid>
+                    {/* SPD */}
+                    <Grid item xs={4}>
+                        <ScrollSelect
+                            options={basicScrollOptions}
+                            label="SPD"
+                            selectedValue={spdScroll}
+                            onChange={handleSpdScrollChange}
+                        />
+                    </Grid>
+                    {/* LUK */}
+                    <Grid item xs={4}>
+                        <ScrollSelect
+                            options={basicScrollOptions}
+                            label="LUK"
+                            selectedValue={lukScroll}
+                            onChange={handleLukScrollChange}
+                        />
+                    </Grid>
+                    <Grid item xs={4}></Grid>
+
+                    {/* ATK */}
+                    <Grid item xs={3}>
+                        <ScrollSelect
+                            options={detailScrollOptions}
+                            label="ATK"
+                            selectedValue={atkScroll}
+                            onChange={handleAtkScrollChange}
+                        />
+                    </Grid>
+                    {/* DEF */}
+                    <Grid item xs={3}>
+                        <ScrollSelect
+                            options={detailScrollOptions}
+                            label="DEF"
+                            selectedValue={defScroll}
+                            onChange={handleDefScrollChange}
+                        />
+                    </Grid>
+                    {/* MAT */}
+                    <Grid item xs={3}>
+                        <ScrollSelect
+                            options={detailScrollOptions}
+                            label="MAT"
+                            selectedValue={matScroll}
+                            onChange={handleMatScrollChange}
+                        />
+                    </Grid>
+                    {/* MDF */}
+                    <Grid item xs={3}>
+                        <ScrollSelect
+                            options={detailScrollOptions}
+                            label="MDF"
+                            selectedValue={mdfScroll}
+                            onChange={handleMdfScrollChange}
+                        />
+                    </Grid>
+                    
                 </Grid>
             </Grid>
 
+            <Grid item xs={1}></Grid>
             {/* リキッド */}
-            <Grid item xs={6}>
-
+            <Grid item xs={5} alignItems={'start'}>
+                <ButtonGroupComponent buttons={liquidButtonsData} />
             </Grid>
         </Grid>
+        </Box>
         </>
     )
 }
